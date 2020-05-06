c1ee528750852666417879a0383c5343b053f30f
elisagc <elisagcubero@gmail.com>
2020-05-06 09:21:25 +0200

    player

--- src/app/components/common/mplayer/mplayer.component.ts
+++ src/app/components/common/mplayer/mplayer.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from "@angular/core";
 import { YtPlayerService, PlayerOptions } from "yt-player-angular";
 import { YoutubeService } from "src/app/services/youtube.service";
+import { SimpleChanges } from "@angular/core";
 
 @Component({
   selector: "app-mplayer",
@@ -15,26 +16,44 @@ export class MplayerComponent implements OnInit {
   @Input() ids: string[];
   @Input() imgGame: string;
   idPlaying: string;
-  autoplay: boolean = true;
   countSong: number = 0;
+  played: boolean = true;
+
+  maxTime;
   timer;
 
+  totalTime;
+  currentTime;
+  saveCurrentTime;
+  @Input() change: number;
   playerOptions: PlayerOptions = {
-    autoplay: false
+    autoplay: true
   };
 
   ngOnInit(): void {
     this.youtubeService.getInfoVideo();
     this.idPlaying = this.ids[0];
-
     this.ytPlayerService.stateChange$.subscribe(state => {
-      this.timer = state.payload;
+      this.timer = Math.trunc(state.payload);
       state.type === 3 ? this.changeSong("next") : null;
+      this.maxTime = this.ytPlayerService.getDuration();
+      //coge la duración total de la canción. Internar no volver a llamar hasta que haya terminado la canción:
+      this.totalTime = this.setSeconds(this.ytPlayerService.getDuration());
+
+      console.log("total time", this.totalTime);
+      //this.currentTime = this.setSeconds(this.timer);
     });
   }
 
   playSong() {
+    this.played = !this.played;
     this.ytPlayerService.play();
+  }
+
+  pauseSong() {
+    this.saveCurrentTime = this.currentTime;
+    this.played = !this.played;
+    this.ytPlayerService.pause();
   }
 
   changeSong(id: string) {
@@ -55,5 +74,18 @@ export class MplayerComponent implements OnInit {
 
   changeId() {
     this.idPlaying = this.ids[this.countSong];
+  }
+
+  setSeconds(seconds) {
+    var hour: any = Math.floor(seconds / 3600);
+    hour = Math.floor(seconds / 3600) < 10 ? "0" + hour : hour;
+    var minute: any = Math.floor((seconds / 60) % 60);
+    minute = minute < 10 ? "0" + minute : minute;
+    var second: any = seconds % 60;
+    console.log("antes", second);
+    second = second < 10 ? "0" + second : second;
+    console.log("después", second);
+    // second = Math.trunc(second);
+    return [hour, minute, second];
   }
 }
